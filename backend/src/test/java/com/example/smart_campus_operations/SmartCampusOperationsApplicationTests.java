071df8e37eade0e55b3eb17022e5a83c4ceffae0
semini24 <semininm19@gmail.com>
2026-04-07 15:00:00 +0530

    added application context test

---
+++ backend/src/test/java/com/example/smart_campus_operations/SmartCampusOperationsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.smart_campus_operations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class SmartCampusOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
